bdcd5635109a16f190b1255e7e5c93bde98eccfc
bvcious10 <bvcious10@gmail.com>
2024-11-21 16:42:46 +0100

    added jenkinsfile

---
+++ client/AKS/main.tf
@@ -0,0 +1,63 @@
+provider "azurerm" {
+  subscription_id = "74ec7602-ef77-4350-81e9-6673c7022a2c"
+  features {}
+}
+
+# Variables 
+variable "resource_group_name" {
+  default = "aks-rg"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "aks_cluster_name" {
+  default = "myAKSCluster"
+}
+
+variable "node_pool_name" {
+  default = "nodepool1"
+}
+
+variable "node_count" {
+  default = 1
+}
+
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# AKS Cluster
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_cluster_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "${var.aks_cluster_name}-dns"
+
+  default_node_pool {
+    name       = var.node_pool_name
+    node_count = var.node_count
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Development"
+  }
+}
+
+# Output the AKS cluster's Kubernetes config
+output "kubeconfig" {
+  value = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
+}
+
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
