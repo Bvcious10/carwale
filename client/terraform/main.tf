17ed75846757395306c03c9dd4ba7366154029ca
bvcious10 <bvcious10@gmail.com>
2024-11-21 15:37:49 +0100

    added terraform file

---
+++ client/terraform/main.tf
@@ -0,0 +1,85 @@
+# Provider configuration
+provider "azurerm" {
+  subscription_id = "74ec7602-ef77-4350-81e9-6673c7022a2c"
+  features {}
+}
+
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "terraform-rg"
+  location = "East US"
+}
+
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "terraform-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "terraform-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Public IP
+resource "azurerm_public_ip" "pip" {
+  name                = "terraform-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
+
+# Network Interface
+resource "azurerm_network_interface" "nic" {
+  name                = "terraform-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "terraform-ip-config"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+# Virtual Machine
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "ubuntu-vm"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  size                = "Standard_D2s_v3"
+  admin_username      = "azureuser"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub") # Path to your SSH public key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  
+}
+
+# Output the public IP address
+output "vm_public_ip" {
+  value = azurerm_public_ip.pip.ip_address
+}
