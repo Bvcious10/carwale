de6195403ea8dc60d008fb1c269926050a353e78
bvcious10 <bvcious10@gmail.com>
2024-11-22 16:13:09 +0100

    changes

--- client/trivy-base-image-scan.sh
+++ client/trivy-base-image-scan.sh
@@ -4,11 +4,11 @@ dockerimage=$(awk 'NR==2 {print $2}' Dockerfile)
 docker run --rm -v $PWD:/myapp aquasec/trivy -q image --exit-code 0 --severity HIGH --light $dockerimage
 docker run --rm -v $PWD:/myapp aquasec/trivy -q image --exit-code 1 --severity CRITICAL --light $dockerimage
 
-exit_code=$?
+# exit_code=$?
 
-if [[ "${exit_code}" == 1]]; then
-    echo "Image scanning failed. Vulnerabilities found"
-    exit 1;
-else
-    echo "Image scanning passed. No vulnerabilities found"
-fi;
+# if [[ "${exit_code}" == 1]]; then
+#     echo "Image scanning failed. Vulnerabilities found"
+#     exit 1;
+# else
+#     echo "Image scanning passed. No vulnerabilities found"
+# fi;
